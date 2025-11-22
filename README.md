881c17b221a1244b2da3aaf47f38686797dec2c1
nkaroly03 <nizderkaroly03@gmail.com>
2025-11-22 23:52:09 +0100

    Finished README, added comments

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 ROS 2 C++ package. [![Static Badge](https://img.shields.io/badge/ROS_2-Humble-34aec5)](https://docs.ros.org/en/humble/)
 
+The package consists of 1 node (/tsp_node). The node is a solver for the Traveling Salesman Problem, which is an np-hard problem. These types of problems don't have a known polynomial solution. The problem asks the following question: "Given a list of cities and the distances between each pair of cities, what is the shortest possible route than visits each city exactly once and returns to the origin city?" There're a lot a of different approaches to solve this problem, like brute forcing, dynamic programming, branch & bound / branch & cut, integer programming, etc. The previously listed techniques always calculate the optimal solution, however the runtime of these algorithms are unacceptable if there're too many cities. In practice we're usually satisfied as long as we find a good enough solution and the calculation is relatively fast. The node itself uses such an algorithm, an evolutionary algorithm.
+
 ## Packages and build
 
 It is assumed that the workspace is `~/ros2_ws/`.
@@ -23,8 +25,13 @@ cd ~/ros2_ws
 ```
 
 ```r
-colcon build --packages-select niz_xvn_tsp --symlink-install
+colcon build --packages-select niz_xvn_tsp
 ```
+
+### Run the package
+
+The node uses csv files filled with x, y coordinates.
+In order to actually run the node you must provide an $\bold{\underline{\text{filepath}}}$ as a parameter with the key name of $"\bold{\underline{\text{file}\_\text{name}}}"$. An example for file_name: "/home/your_user_name/ros2_ws/src/niz_xvn_tsp/csv/test1.csv"
 
 <details>
 <summary> Don't forget to source before ROS commands.</summary>
@@ -36,28 +43,64 @@ source ~/ros2_ws/install/setup.bash
 </details>
 
 ```r
-ros2 launch niz_xvn_tsp launch_example1.launch.py
+ros2 run niz_xvn_tsp tsp_node --ros-args -p file_name:="/home/your_user_name/ros2_ws/src/niz_xvn_tsp/csv/test1.csv"
 ```
 
-# Delete this part if you are using it as a template
+Also, there's an optional parameter $"\bold{\underline{\text{population}\_\text{size}}}"$, which is an and unsigned integer type that, if not specified, defaults to the number of coordinates found in the csv file. You can supply this parameter if you believe it'll have a positive impact on the result.
+With the optional parameter it would look like this:
 
-ROS 2 pacage template, to get started, use template by clicking on the Green button labeled [`Use this template`](https://github.com/nkaroly03/niz_xvn_tsp/generate) / [`Create new repository`](https://github.com/nkaroly03/niz_xvn_tsp/generate).
+<details>
+<summary> Don't forget to source before ROS commands.</summary>
 
-<p align="center"><img src="img/use_this_template01.png" width="60%" /></p>
+```bash
+source ~/ros2_ws/install/setup.bash
+```
 
-Let's assume
+</details>
 
-- your Github username is `mycoolusername`
-- your ROS 2 repo shold be `cool_ros2_package`
+```r
+ros2 run niz_xvn_tsp tsp_node --ros-args -p file_name:="/home/your_user_name/ros2_ws/src/niz_xvn_tsp/csv/test1.csv" -p population_size:=50
+```
 
-Replace everything in the cloned repo:
+As soon as the program starts the solving algorithm also starts.
 
-- `niz_xvn_tsp` >> `cool_ros2_package` (the folder was already renamed after `Use this template`)
-- `nkaroly03` >> `mycoolusername`
-- find all `todo` strings and fill the blanks
+Open rviz in a separate terminal, and add the MarkerArray topic under /marker_array_topic:
 
-The easiest way is VS code:
+<details>
+<summary> Don't forget to source before ROS commands.</summary>
 
-<p align="center"><img src="img/replace01.png" width="60%" /></p>
+```bash
+source ~/ros2_ws/install/setup.bash
+```
 
-Now `colcon build` your ROS 2 package and you can start wokring.
+</details>
+
+```r
+ros2 run rviz2 rviz2
+```
+
+![](img/menu.png)
+
+Because the wall_timer time is set to 1ns it basically shows the greatest solutions it has found. If you also want to see how the solution gets better over time then increase the 1ns duration in the source code and rebuild the package. The time cannot be parameterized because in practice you would want to get the greatest result as soon as possible.
+
+![](img/result.png)
+
+## Diagram
+
+Made with [Mermaid diagram](https://mermaid.js.org/intro/):
+
+```mermaid
+flowchart LR
+
+A{{file_name</br>param}}:::gray --> D([/tsp_node]):::red
+B{{population_size</br>param}}:::gray --> D
+D --> |visualization_msgs/marker_array| P[ /marker_array_topic]:::light
+
+
+classDef light fill:#34aec5,stroke:#152742,stroke-width:2px,color:#152742
+classDef dark fill:#152742,stroke:#34aec5,stroke-width:2px,color:#34aec5
+classDef white fill:#ffffff,stroke:#152742,stroke-width:2px,color:#152742
+classDef gray fill:#f6f8fa,stroke:#152742,stroke-width:2px,color:#152742
+classDef red fill:#ef4638,stroke:#152742,stroke-width:2px,color:#fff
+
+```
--- src/tsp.cpp
+++ src/tsp.cpp
@@ -11,11 +11,13 @@
 class Solver : public rclcpp::Node{
     private:
     struct Vec2{ double x, y; };
+    // stores coordinate idxs and the associated cost of the path traveled going from idx to idx
     struct Population{
-        std::unique_ptr<size_t[]> ids;
+        std::unique_ptr<size_t[]> idxs;
         double cost;
     };
 
+    // chance to mutate m_pop_buf after crossover
     static constexpr double M_MUTATION_RATIO = 0.2;
 
     std::string m_file_name;
@@ -45,6 +47,7 @@ class Solver : public rclcpp::Node{
         while ((file >> x).get(c) >> y)
             m_coors.push_back(Vec2{x, y});
     }
+    // calculates the distance between each city pair
     void calculate_distances(){
         m_distances = std::make_unique<double[]>(m_coors.size() * m_coors.size());
 
@@ -53,25 +56,27 @@ class Solver : public rclcpp::Node{
             for (const auto &v2 : m_coors)
                 *it++ = std::hypot(v2.x - v1.x, v2.y - v1.y);
     }
-
-    double calculate_cost(std::unique_ptr<size_t[]> &ids) noexcept{
+    
+    double calculate_cost(std::unique_ptr<size_t[]> &idxs) noexcept{
         double cost = 0.0;
         for (size_t i = 0; i < m_coors.size() - 1; ++i)
-            cost += m_distances[ids[i] * m_coors.size() + ids[i + 1]];
+            cost += m_distances[idxs[i] * m_coors.size() + idxs[i + 1]];
         return cost;
     }
+    // creates a starting population
     void create_population(){
         m_pops = std::make_unique<Population[]>(m_population_size);
         for (auto it = &m_pops[0]; it != &m_pops[m_population_size]; ++it){
-            it->ids = std::make_unique<size_t[]>(m_coors.size());
+            it->idxs = std::make_unique<size_t[]>(m_coors.size());
             for (size_t i = 0; i < m_coors.size(); ++i)
-                it->ids[i] = i;
-            std::shuffle(&it->ids[0], &it->ids[m_coors.size()], m_rand);
-            it->cost = calculate_cost(it->ids);
+                it->idxs[i] = i;
+            std::shuffle(&it->idxs[0], &it->idxs[m_coors.size()], m_rand);
+            it->cost = calculate_cost(it->idxs);
         }
         std::sort(&m_pops[0], &m_pops[m_population_size], [](const Population &p1, const Population &p2){ return p1.cost < p2.cost; });
     }
 
+    // 1 point crossover between 2 populations into m_pop_buf
     void crossover() noexcept{
         size_t population_size_half = m_population_size / 2;
         size_t pop_idx1 = m_rand() % population_size_half, pop_idx2 = population_size_half + m_rand() % population_size_half;
@@ -81,21 +86,22 @@ class Solver : public rclcpp::Node{
         const Population &p1 = m_pops[pop_idx1], &p2 = m_pops[pop_idx2];
 
         size_t crossover_point = m_rand() % (m_coors.size() + 1);
-        std::copy(&p1.ids[0], &p1.ids[crossover_point], &m_pop_buf.ids[0]);
+        std::copy(&p1.idxs[0], &p1.idxs[crossover_point], &m_pop_buf.idxs[0]);
 
         size_t i = 0;
         while (crossover_point < m_coors.size()){
-            if (std::find(&m_pop_buf.ids[0], &m_pop_buf.ids[crossover_point], p2.ids[i]) == &m_pop_buf.ids[crossover_point])
-                m_pop_buf.ids[crossover_point++] = p2.ids[i];
+            if (std::find(&m_pop_buf.idxs[0], &m_pop_buf.idxs[crossover_point], p2.idxs[i]) == &m_pop_buf.idxs[crossover_point])
+                m_pop_buf.idxs[crossover_point++] = p2.idxs[i];
             ++i;
         }
 
-        m_pop_buf.cost = calculate_cost(m_pop_buf.ids);
+        m_pop_buf.cost = calculate_cost(m_pop_buf.idxs);
     }
+    // mutate m_pop_buf by randomly deciding to switch 2 random idxs
     void mutate() noexcept{
         if (static_cast<double>(m_rand() % 100) < M_MUTATION_RATIO * 100.0){
-            std::swap(m_pop_buf.ids[m_rand() % m_coors.size()], m_pop_buf.ids[m_rand() % m_coors.size()]);
-            m_pop_buf.cost = calculate_cost(m_pop_buf.ids);
+            std::swap(m_pop_buf.idxs[m_rand() % m_coors.size()], m_pop_buf.idxs[m_rand() % m_coors.size()]);
+            m_pop_buf.cost = calculate_cost(m_pop_buf.idxs);
         }
     }
 
@@ -103,29 +109,32 @@ class Solver : public rclcpp::Node{
         crossover();
         mutate();
 
+        // found a better solution
         if (m_pop_buf.cost < m_pops[0].cost){
-            std::copy(&m_pop_buf.ids[0], &m_pop_buf.ids[m_coors.size()], &m_pops[0].ids[0]);
+            std::copy(&m_pop_buf.idxs[0], &m_pop_buf.idxs[m_coors.size()], &m_pops[0].idxs[0]);
             m_pops[0].cost = m_pop_buf.cost;
         }
 
+        // switch a weak population with the 1 in m_pop_buf and calculate an immigrant (fully random) solution
         size_t population_size_half = m_population_size / 2;
         std::swap(m_pop_buf, m_pops[population_size_half + m_rand() % population_size_half]);
-        std::shuffle(&m_pop_buf.ids[0], &m_pop_buf.ids[m_coors.size()], m_rand);
-        m_pop_buf.cost = calculate_cost(m_pop_buf.ids);
+        std::shuffle(&m_pop_buf.idxs[0], &m_pop_buf.idxs[m_coors.size()], m_rand);
+        m_pop_buf.cost = calculate_cost(m_pop_buf.idxs);
 
         if (m_pop_buf.cost < m_pops[0].cost){
-            std::copy(&m_pop_buf.ids[0], &m_pop_buf.ids[m_coors.size()], &m_pops[0].ids[0]);
+            std::copy(&m_pop_buf.idxs[0], &m_pop_buf.idxs[m_coors.size()], &m_pops[0].idxs[0]);
             m_pops[0].cost = m_pop_buf.cost;
         }
 
+        // swap again
         std::swap(m_pop_buf, m_pops[population_size_half + m_rand() % population_size_half]);
-        std::shuffle(&m_pop_buf.ids[0], &m_pop_buf.ids[m_coors.size()], m_rand);
-        m_pop_buf.cost = calculate_cost(m_pop_buf.ids);
+        std::shuffle(&m_pop_buf.idxs[0], &m_pop_buf.idxs[m_coors.size()], m_rand);
+        m_pop_buf.cost = calculate_cost(m_pop_buf.idxs);
 
         std::sort(&m_pops[0], &m_pops[m_population_size], [](const Population &p1, const Population &p2){ return p1.cost < p2.cost; });
 
         visualization_msgs::msg::MarkerArray markers;
-        visualization_msgs::msg::Marker spheres, lines;
+        visualization_msgs::msg::Marker spheres, lines, text;
 
         spheres.header.frame_id = "map";
         spheres.ns = "spheres";
@@ -136,11 +145,9 @@ class Solver : public rclcpp::Node{
         spheres.pose.orientation.y = 0.0;
         spheres.pose.orientation.z = 0.0;
         spheres.pose.orientation.w = 0.0;
-
         spheres.scale.x = 0.2;
         spheres.scale.y = 0.2;
         spheres.scale.z = 0.2;
-        
         spheres.color.r = 1.0f;
         spheres.color.g = 0.0f;
         spheres.color.b = 0.0f;
@@ -155,17 +162,32 @@ class Solver : public rclcpp::Node{
         lines.pose.orientation.y = 0.0;
         lines.pose.orientation.z = 0.0;
         lines.pose.orientation.w = 0.0;
-
         lines.scale.x = 0.1;
-        
         lines.color.r = 0.0f;
         lines.color.g = 0.0f;
         lines.color.b = 1.0f;
         lines.color.a = 1.0f;
 
+        text.header.frame_id = "map";
+        text.ns = "text";
+        text.type = visualization_msgs::msg::Marker::TEXT_VIEW_FACING;
+        text.id = 2;
+        text.action = visualization_msgs::msg::Marker::ADD;
+        text.pose.orientation.x = 0.0;
+        text.pose.orientation.y = 0.0;
+        text.pose.orientation.z = 0.0;
+        text.pose.orientation.w = 0.0;
+        text.scale.x = 1.0;
+        text.scale.y = 1.0;
+        text.scale.z = 1.0;
+        text.color.r = 0.0f;
+        text.color.g = 1.0f;
+        text.color.b = 0.0f;
+        text.color.a = 1.0f;
+
         geometry_msgs::msg::Point p;
         p.z = 0.0;
-        for (auto it = &m_pops[0].ids[0]; it != &m_pops[0].ids[m_coors.size()]; ++it){
+        for (auto it = &m_pops[0].idxs[0]; it != &m_pops[0].idxs[m_coors.size()]; ++it){
             p.x = m_coors[*it].x;
             p.y = m_coors[*it].y;
 
@@ -173,12 +195,15 @@ class Solver : public rclcpp::Node{
             lines.points.push_back(p);
         }
 
-        p.x = m_coors[m_pops[0].ids[0]].x;
-        p.y = m_coors[m_pops[0].ids[0]].y;
+        p.x = m_coors[m_pops[0].idxs[0]].x;
+        p.y = m_coors[m_pops[0].idxs[0]].y;
         lines.points.push_back(p);
+
+        text.text = "Cost: " + std::to_string(m_pops[0].cost);
 
         markers.markers.push_back(spheres);
         markers.markers.push_back(lines);
+        markers.markers.push_back(text);
 
         // for (size_t i = 0; i < m_population_size; ++i)
             // RCLCPP_INFO(this->get_logger(), "%lf", m_pops[0].cost);
@@ -203,7 +228,7 @@ class Solver : public rclcpp::Node{
         m_rand.seed(rd());
 
         create_population();
-        m_pop_buf.ids = std::make_unique<size_t[]>(m_coors.size());
+        m_pop_buf.idxs = std::make_unique<size_t[]>(m_coors.size());
 
         m_publisher = this->create_publisher<visualization_msgs::msg::MarkerArray>("marker_array_topic", 10);
         m_timer = this->create_wall_timer(1ns, std::bind(&Solver::timer_callback, this));
