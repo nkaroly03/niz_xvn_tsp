36144824c818eccb68ece146f3735d919ad5da09
nkaroly03 <nizderkaroly03@gmail.com>
2025-11-22 19:50:13 +0100

    Finished Solver initializer

--- src/tsp.cpp
+++ src/tsp.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 #include <fstream>
 #include <memory>
+#include <random>
 #include <string>
 #include <vector>
 
@@ -16,11 +17,12 @@ class Solver : public rclcpp::Node{
     };
 
     std::string m_file_name;
+    std::mt19937_64 m_rand;
     std::vector<Vec2> m_coors;
     std::unique_ptr<double[]> m_distances;
     size_t m_population_size;
     std::unique_ptr<Population[]> m_pops;
-    Population m_buf;
+    Population m_pop_buf;
 
     rclcpp::TimerBase::SharedPtr m_timer;
     rclcpp::Publisher<visualization_msgs::msg::MarkerArray>::SharedPtr m_publisher;
@@ -45,9 +47,27 @@ class Solver : public rclcpp::Node{
         m_distances = std::make_unique<double[]>(m_coors.size() * m_coors.size());
 
         double *it = m_distances.get();
-        for (const Vec2 &v1 : m_coors)
-            for (const Vec2 &v2 : m_coors)
+        for (const auto &v1 : m_coors)
+            for (const auto &v2 : m_coors)
                 *it++ = std::hypot(v2.x - v1.x, v2.y - v1.y);
+    }
+
+    double calculate_cost(std::unique_ptr<size_t[]> &ids) noexcept{
+        double cost = 0.0;
+        for (size_t i = 0; i < m_coors.size() - 1; ++i)
+            cost += m_distances[ids[i] * m_coors.size() + ids[i + 1]];
+        return cost;
+    }
+    void create_population(){
+        m_pops = std::make_unique<Population[]>(m_population_size);
+        for (auto it = &m_pops[0]; it != &m_pops[m_population_size]; ++it){
+            it->ids = std::make_unique<size_t[]>(m_coors.size());
+            for (size_t i = 0; i < m_coors.size(); ++i)
+                it->ids[i] = i;
+            std::shuffle(&it->ids[0], &it->ids[m_coors.size()], m_rand);
+            it->cost = calculate_cost(it->ids);
+        }
+        std::sort(&m_pops[0], &m_pops[m_population_size], [](const Population &p1, const Population &p2){ return p1.cost < p2.cost; });
     }
 
     void timer_callback(){
@@ -65,6 +85,12 @@ class Solver : public rclcpp::Node{
 
         this->declare_parameter("population_size", static_cast<int64_t>(m_coors.size()));
         this->get_parameter("population_size", m_population_size);
+
+        std::random_device rd;
+        m_rand.seed(rd());
+
+        create_population();
+        m_pop_buf.ids = std::make_unique<size_t[]>(m_coors.size());
 
         m_publisher = this->create_publisher<visualization_msgs::msg::MarkerArray>("marker_array_topic", 10);
         m_timer = this->create_wall_timer(500ms, std::bind(&Solver::timer_callback, this));
